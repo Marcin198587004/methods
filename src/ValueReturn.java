68221403b53b651c9d86d599eed8cd79077eb4ba
marcinkaminski <marcinkaminski.1@o2.pl>
2021-07-03 15:12:03 +0200

    metodhs returns value

---
+++ src/ValueReturn.java
@@ -0,0 +1,28 @@
+/**
+ * program ilustruje metode zwracajacą wartosc
+ */
+public class ValueReturn {
+    public static void main(String[] args) {
+        int total;
+        int value1 = 20;
+        int value2 = 40;
+
+        total = sum(value1, value2);
+
+        System.out.println("Suma liczb " + value1 + " i " + value2 + " wynosi " + total + ".");
+    }
+
+    /**
+     * Metoda sum zwraca sume dwoch parametrow
+     * @param num1 Pierwsza z dodawanych liczb
+     ** @param num2 Druga z dodawanych liczb
+     * @return Suma parametrów num1 i num2
+     */
+   public static int sum(int num1,int num2)
+{
+    int result;
+    result = num1 + num2;
+    return result;
+
+    }
+}
