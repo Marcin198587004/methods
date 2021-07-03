c9710f81732e86935da2fb0ee7b2fe45f34b15b5
marcinkaminski <marcinkaminski.1@o2.pl>
2021-07-03 11:21:55 +0200

    metodhs

---
+++ src/myName.java
@@ -0,0 +1,17 @@
+public class myName {
+    public static void main(String[] args) {
+
+       String name = "Marcin";
+        System.out.println(name);
+        myName();
+        System.out.println(name);
+    }
+
+    public static void myName()
+
+    {
+        String name = "Marcin Kamiński";
+        System.out.println(name);
+    }
+}
+
